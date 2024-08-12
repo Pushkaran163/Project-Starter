cadc8eeffb9a2d5f2b92afccc4347f1c238a84b0
Pushkaran_Tyagi163 <117625210+Pushkaran163@users.noreply.github.com>
2024-08-12 23:21:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('clickMe');
+    
+    button.addEventListener('click', () => {
+        alert('Button was clicked!');
+    });
+});
